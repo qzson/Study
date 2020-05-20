3e1138fe681cc14faefa086b42b8996a1a92dee9
qzson <qzson@naver.com>
2020-05-20 20:39:33 +0900

    range 클래스

---
+++ python/python07_range.py
@@ -0,0 +1,19 @@
+# 200520 11:22~
+# range 클래스
+
+a = range(10)
+print(a)          # range(0, 10)
+b = range(1, 11)
+print(b)          # range(1, 11)
+
+for i in a:
+    print(i)      # 0 ~ 9
+for i in b:
+    print(i)      # 1 ~ 10
+
+print(type(a))    # <class 'range'>
+
+sum = 0
+for i in range(1, 11):
+    sum = sum + i
+print(sum)
