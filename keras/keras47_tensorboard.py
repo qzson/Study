bcc9f6fba7cc5eae5bdce267501846cbc877c139
qzson <qzson@naver.com>
2020-05-28 20:55:01 +0900

    텐서보드 웹 활용 수정

--- keras/keras47_tensorboard.py
+++ keras/keras47_tensorboard.py
@@ -1,5 +1,6 @@
 # 200525 1600~
 # tensorboard 사용 웹 그래프 표현법
+# lstm 구성
 
 
 import numpy as np
@@ -11,7 +12,7 @@ a = np.array(range(1, 101))
 size = 5
 
 def split_x(seq, size):
-    aaa = []        # 는 리스트
+    aaa = []
     for i in range(len(seq) - size + 1):
         subset = seq[i : (i+size)]
         aaa.append([item for item in subset])
@@ -19,17 +20,18 @@ def split_x(seq, size):
     return np.array(aaa)
 
 dataset = split_x(a, size)
-# print(dataset)
-# print(dataset.shape)
+print(dataset)
+print(dataset.shape)                # (96, 5)
 
 x = dataset[:, 0:4]                 # : 모든 행, 그 다음 0:4
 y = dataset[:, 4]                   # : 모든 행, 인덱스 4부분만 가져오겠다.
 
-print(x.shape)
-print(y.shape)
+print(x.shape)                      # (96, 4)
+print(y.shape)                      # (96, )
+print(y)
 
 x = np.reshape(x, (96,4,1))
-print(x.shape)
+print(x.shape)                      # (96, 4, 1)
 
 
 # 2. 모델
@@ -43,38 +45,43 @@ model.add(Dense(1))
 model.summary()
 
 
+# EarlyStopping
+from keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='loss', patience=5, mode='auto')
+
 # 3. 실행
 model.compile(loss='mse', optimizer='adam', metrics = ['acc'] )
 
-# 3-1. 얼리스타핑
-from keras.callbacks import EarlyStopping, TensorBoard
+# Tensorboard
+from keras.callbacks import TensorBoard
 tb_hist = TensorBoard(log_dir='graph', histogram_freq=0,    # graph라고 써주고 어떻게 이용되는지 본다.
-                      write_graph=True, write_images=True)
-early_stopping = EarlyStopping(monitor='loss', patience=5, mode='auto')
+                      write_graph=True, write_images=True)  # log_dir=' 폴더 ' : 제일 많이 틀림
+ # (cmd에서) -> d: -> cd study -> cd graph -> tensorboard --logdir=.
+ # 127.2.0.0 // 6006 포트를 사용하겠다. = 127.0.0.1:6006 이것을 웹에 붙여넣기해서 이동
 
-# cmd -> d: -> cd study -> cd graph -> tensorboard --logdir=.
-# 127.2.0.0 // 6006 포트를 사용하겠다. = 127.0.0.1:6006/
-
-# 3-2. 훈련
+# 3. 훈련
 hist = model.fit(x, y, epochs=100, batch_size=1, verbose=2,
           validation_split=0.2,
-          callbacks=[early_stopping, tb_hist])
+          callbacks=[es, tb_hist])
+ # Tensorboard 사용
+ # hist = model.fit에 훈련시키고 난 loss, metrics안에 있는 값들을 반환한다.
+
 
 # print(hist)                # 자료형만 출력
 # print(hist.history.keys()) # dict_keys(['loss', 'mse']) 키 loss와 mse가 있는데 각각 벨류도 있을 것이다.
 
 import matplotlib.pyplot as plt
 
-plt.plot(hist.history['loss'])      # 히스토리에 저장된 애들을 끄집어온다.
-plt.plot(hist.history['val_loss'])  # x,y값 둘다 드가거나 y값 집어넣거나
+plt.plot(hist.history['loss'])       # 'loss'값을 y로 넣겠다./ 인자 하나만 쓰면 y 값으로 들어감
+plt.plot(hist.history['val_loss'])   # 시간에 따른 loss, acc여서 x 값으로는 자연스럽게 epoch가 들어감
 plt.plot(hist.history['acc'])
 plt.plot(hist.history['val_acc'])
 plt.title('loss & acc')
 plt.ylabel('loss, acc')
 plt.xlabel('epoch')
-plt.legend(['train loss', 'val loss', 'train acc', 'val acc'])
+plt.legend(['train loss', 'val loss', 'train acc', 'val acc'])     # 선에 대한 색깔과 설명이 나옴
 # 검증이 실질적으로 좋지 않게 나왔다. (val 그래프가 위로 치고 있으니)
-# plt.show() 지금은 필요 x
+# plt.show()
 
 
 '''
