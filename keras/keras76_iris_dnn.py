dc98a6609f23c826054fda054f676307642051c7
qzson <qzson@naver.com>
2020-05-29 20:02:29 +0900

    iris dnn

---
+++ keras/keras76_iris_dnn.py
@@ -0,0 +1,133 @@
+import numpy as np
+from sklearn.datasets import load_iris
+
+dataset = load_iris()
+x = dataset.data
+y = dataset.target
+
+print(x.shape) # (150, 4)
+print(y.shape) # (150,)
+
+###
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaler.fit(x)
+x_scaled = scaler.transform(x)
+# print(x_scaled)
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+pca.fit(x_scaled)
+x_pca = pca.transform(x_scaled)
+# print(x_pca)
+print(x_pca.shape)   #(150, 2)
+
+###
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x_pca, y, random_state=66, shuffle=True,
+    train_size = 0.8)
+
+print(x_train.shape) #(120, 2)
+print(x_test.shape)  #(30, 2)
+print(y_train.shape) #(120,)
+print(y_test.shape)  #(30,)
+
+### 데이터 전처리 1. OneHotEncoding
+from keras.utils import np_utils
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+print(y_train.shape) #(120, 3)
+print(y_test.shape)  #(30, 3)
+
+### 2. 모델
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+model = Sequential()
+
+model.add(Dense(50, activation='relu', input_shape = (2, )))
+model.add(Dense(100, activation='relu'))
+# model.add(Dropout(0.2))
+
+model.add(Dense(200, activation='relu'))
+model.add(Dense(300, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(300, activation='relu'))
+model.add(Dense(200, activation='relu'))
+model.add(Dense(100, activation='relu'))
+# model.add(Dropout(0.2))
+
+model.add(Dense(3, activation='softmax'))
+
+model.summary()
+
+
+##### EarlyStopping & Modelcheckpoint
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='loss', patience=50)
+
+modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss',
+                     save_best_only=True, mode='auto')
+
+
+##### 3. 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+hist = model.fit(x_train, y_train,
+          epochs=400, batch_size=32, verbose=2,
+          validation_split=0.4,
+          callbacks=[es, cp])
+
+
+##### 4. 평가, 예측
+loss_acc = model.evaluate(x_test, y_test, batch_size=32)
+
+loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+acc = hist.history['acc']
+val_acc = hist.history['val_acc']
+
+print('acc 는 ', acc)
+print('val_acc 는 ', val_acc)
+
+# evaluate 종속 결과
+print('loss, acc 는 ', loss_acc)
+
+
+##### plt 시각화
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,6))
+
+# 1
+plt.subplot(2,1,1)
+plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
+plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
+plt.grid()
+plt.title('loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(loc='upper right')
+
+# 2
+plt.subplot(2,1,2)
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.grid()
+plt.title('acc')
+plt.ylabel('acc')
+plt.xlabel('epoch')
+plt.legend(['acc', 'val_acc'])
+
+plt.show()
+
+'''
+loss, acc 는  [0.660108745098114, 0.8333333134651184]
+'''
