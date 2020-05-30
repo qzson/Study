67bdbf3d32f2a1e95d1c812949355b8eab0af696
qzson <qzson@naver.com>
2020-05-31 00:48:19 +0900

    미완성

--- keras/keras83_breast_cacer_lstm.py
+++ keras/keras83_breast_cacer_lstm.py
@@ -1,9 +1,12 @@
+# 20-05-31 / 일요일
+
+# 이건 이진 분류 같음
+
+
+### 1. 데이터
 import numpy as np
 from sklearn.datasets import load_breast_cancer
-
-dataset = load_breast_cancer()
-x = dataset.data
-y = dataset.target
-
+x, y = load_breast_cancer(return_X_y=True)
+# print(x[0])
 print(x.shape) # (569, 30)
 print(y.shape) # (569,)
