7e3bf924e5bad57e7a1b7e7b7fa9735bac85e173
qzson <qzson@naver.com>
2020-06-08 23:03:16 +0900

    전처리의 친구 Pipeline

---
+++ ml/m13_pipeline.py
@@ -0,0 +1,34 @@
+# 20-06-08_20
+# 월요일 // 14:00 ~
+
+# Pipeline
+# 전처리의 친구
+
+
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+
+''' 1. 데이터 '''
+iris = load_iris()
+
+x = iris.data
+y = iris.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 43, shuffle = True)
+
+
+''' 2. 모델 '''
+# model = SVC()
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+                                                                  # 전처리와 model이 한번에 돌아감
+pipe = Pipeline([("scaler", MinMaxScaler()), ('svm', SVC())])     # 쓸 scaler와 model을 명시
+
+pipe.fit(x_train, y_train)
+
+print('acc :', pipe.score(x_test, y_test))
