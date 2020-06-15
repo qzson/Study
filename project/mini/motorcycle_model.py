f05e6f652a7cf520622b1b994bfd08ef1337041d
qzson <qzson@naver.com>
2020-06-15 09:40:22 +0900

    미니 프로젝트 성공적인 모델 구현

--- project/mini/motorcycle_model.py
+++ project/mini/motorcycle_model.py
@@ -40,8 +40,14 @@ model.summary()
 
 
 ''' 3. 훈련 '''
+# earlystopping & modelcheckpoint
+es = EarlyStopping(monitor='val_loss', patience=7, mode='auto')
+
+modelpath = './project/mini/checkpoint/cp-{epoch:02d}-{val_loss:.4f}.hdf5'
+cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False)
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-history = model.fit(x_train, y_train, epochs=50, batch_size=8, verbose=1, validation_split=0.2)
+hist = model.fit(x_train, y_train, epochs=40, batch_size=8, verbose=1, validation_split=0.2, callbacks=[es, cp])
 
 
 ''' 4. 평가, 예측 '''
@@ -49,16 +55,35 @@ loss, acc = model.evaluate(x_test, y_test, batch_size=8)
 print("loss :", loss)
 print("acc :", acc)
 
+# pyplot 시각화
+y_vloss = hist.history['val_loss']
+y_loss = hist.history['loss']
+y_vacc = hist.history['val_acc']
+y_acc = hist.history['acc']
 
-y_vloss = history.history['val_loss']
-y_loss = history.history['loss']
+x_len1 = np.arange(len(y_loss))
+x_len2 = np.arange(len(y_acc))
+plt.figure(figsize=(6,6))
 
-x_len = np.arange(len(y_loss))
-
-plt.plot(x_len, y_vloss, marker='.', c='red', label='val_set_loss')
-plt.plot(x_len, y_loss, marker='.', c='blue', label='train_set_oss')
+# 1 Loss 그래프
+plt.subplot(2,1,1)
+plt.plot(x_len1, y_vloss, marker='.', c='red', label='val_loss')
+plt.plot(x_len1, y_loss, marker='.', c='blue', label='loss')
 plt.legend()
+plt.title('Loss')
 plt.xlabel('epochs')
 plt.ylabel('loss')
 plt.grid()
+
+# 2 Acc 그래프
+plt.subplot(2,1,2)
+plt.plot(x_len2, y_vacc, marker='.', c='orange', label='val_acc')
+plt.plot(x_len2, y_acc, marker='.', c='purple', label='acc')
+plt.legend()
+plt.title('Acc')
+plt.xlabel('epochs')
+plt.ylabel('acc')
+plt.grid()
+
+plt.subplots_adjust(hspace=0.4)
 plt.show()
