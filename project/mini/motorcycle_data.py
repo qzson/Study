e484f09c1fefffde357e6fe43a50177c43ab2166
qzson <qzson@naver.com>
2020-06-15 11:32:32 +0900

    미니 프로젝트 데이터

--- project/mini/motorcycle_data.py
+++ project/mini/motorcycle_data.py
@@ -1,16 +1,21 @@
+# 이미지 데이터 처리
+
 from PIL import Image
 import os, glob, numpy as np
 from sklearn.model_selection import train_test_split
 
+
+### 이미지 파일 불러오기 및 카테고리 정의
 caltech_dir = './project/mini/images'
 categories = ['scooter', 'supersports', 'multipurpose', 'cruiser']
 nb_classes = len(categories)
 
+### 가로, 세로, 채널 쉐이프 정의
 image_w = 100
 image_h = 100
-
 pixels = image_h * image_w * 3
 
+### 이미지 파일 Data화
 X = []
 Y = []
 
@@ -38,9 +43,11 @@ y = np.array(Y)
 print(x.shape) # (200, 100, 100, 3)
 print(y.shape) # (200, 4)
 
+### 데이터 train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8)
 xy = (x_train, x_test, y_train, y_test)
 # np.save('./project/mini/data/multi_image_data.npy', xy)
+
 print(x_train.shape)
 print(x_test.shape)
 print(y_train.shape)
