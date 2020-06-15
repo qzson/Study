961c03e883349b60eb79c87b21aaafa495ee6afd
qzson <qzson@naver.com>
2020-06-15 11:32:42 +0900

    미니 프로젝트 CNN 구현

--- project/mini/motorcycle_model.py
+++ project/mini/motorcycle_model.py
@@ -1,3 +1,5 @@
+# CNN 모델 구성 및 checkpoint 저장
+
 import os, glob, numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
@@ -40,7 +42,7 @@ model.summary()
 
 
 ''' 3. 훈련 '''
-# earlystopping & modelcheckpoint
+## earlystopping & modelcheckpoint
 es = EarlyStopping(monitor='val_loss', patience=7, mode='auto')
 
 modelpath = './project/mini/checkpoint/cp-{epoch:02d}-{val_loss:.4f}.hdf5'
@@ -50,12 +52,12 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc']
 hist = model.fit(x_train, y_train, epochs=40, batch_size=8, verbose=1, validation_split=0.2, callbacks=[es])
 
 
-''' 4. 평가, 예측 '''
+''' 4. 평가 '''
 loss, acc = model.evaluate(x_test, y_test, batch_size=8)
 print("loss :", loss)
 print("acc :", acc)
 
-# pyplot 시각화
+## pyplot 시각화
 y_vloss = hist.history['val_loss']
 y_loss = hist.history['loss']
 y_vacc = hist.history['val_acc']
@@ -65,7 +67,7 @@ x_len1 = np.arange(len(y_loss))
 x_len2 = np.arange(len(y_acc))
 plt.figure(figsize=(6,6))
 
-# 1 Loss 그래프
+## 1 Loss 그래프
 plt.subplot(2,1,1)
 plt.plot(x_len1, y_vloss, marker='.', c='red', label='val_loss')
 plt.plot(x_len1, y_loss, marker='.', c='blue', label='loss')
@@ -75,7 +77,7 @@ plt.xlabel('epochs')
 plt.ylabel('loss')
 plt.grid()
 
-# 2 Acc 그래프
+## 2 Acc 그래프
 plt.subplot(2,1,2)
 plt.plot(x_len2, y_vacc, marker='.', c='orange', label='val_acc')
 plt.plot(x_len2, y_acc, marker='.', c='purple', label='acc')
