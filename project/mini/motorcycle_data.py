27a3653ff98df85837dfa22fe101f418a9edf597
qzson <qzson@naver.com>
2020-06-16 14:23:39 +0900

    미니 프로젝트 데이터

--- project/mini/motorcycle_data.py
+++ project/mini/motorcycle_data.py
@@ -13,7 +13,6 @@ nb_classes = len(categories)
 ### 가로, 세로, 채널 쉐이프 정의
 image_w = 100
 image_h = 100
-pixels = image_h * image_w * 3
 
 ### 이미지 파일 Data화
 X = []
@@ -40,17 +39,17 @@ for idx, cat in enumerate(categories):
 x = np.array(X)
 y = np.array(Y)
 
-# print(x.shape) # (200, 100, 100, 3)
-# print(y.shape) # (200, 4)
+print(x.shape) # (200, 100, 100, 3)
+print(y.shape) # (200, 4)
 
-### 데이터 train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8)
-xy = (x_train, x_test, y_train, y_test)
-# print(x_train.shape)    # (160, 100, 100, 3)
-# print(x_test.shape)     # (40, 100, 100, 3)
-# print(y_train.shape)    # (160, 4)
-# print(y_test.shape)     # (40, 4)
+# ### 데이터 train_test_split
+# x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8)
+# xy = (x_train, x_test, y_train, y_test)
+# # print(x_train.shape)    # (160, 100, 100, 3)
+# # print(x_test.shape)     # (40, 100, 100, 3)
+# # print(y_train.shape)    # (160, 4)
+# # print(y_test.shape)     # (40, 4)
 
-### 데이터 SAVE
-np.save('./project/mini/data/multi_image_data.npy', xy)
+# ### 데이터 SAVE
+# # np.save('./project/mini/data/multi_image_data.npy', xy)
 print('ok', len(y))
