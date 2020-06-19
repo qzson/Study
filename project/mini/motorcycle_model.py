e5d9c4b290a1a70a202a3967fdb34ffff3a2faf9
qzson <qzson@naver.com>
2020-06-19 11:58:10 +0900

    미니 프로젝트 모델

--- project/mini/motorcycle_model.py
+++ project/mini/motorcycle_model.py
@@ -33,7 +33,7 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 
 model.add(Dense(nb_classes, activation='softmax'))
