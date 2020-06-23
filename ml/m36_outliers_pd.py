13dac2e9d90025983a11e901d802067b8286d71f
qzson <qzson@naver.com>
2020-06-23 23:03:01 +0900

    이상치 체크 함수 최종판

--- ml/m36_outliers_pd.py
+++ ml/m36_outliers_pd.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 # iloc
-def outliers(data_out):
+def outliers_iloc(data_out):
     out = []
     for col in range(data_out.shape[1]):
         data = data_out.iloc[:, col]
@@ -20,8 +20,8 @@ def outliers(data_out):
         out.append(ans)
     return out
 
-# ioc
-def outliers2(data_out):
+# loc
+def outliers_loc(data_out):
     out = []
     for col in data_out.columns:
         data = data_out.loc[:, col]
@@ -35,6 +35,54 @@ def outliers2(data_out):
         out.append(ans)
     return out
 
+# how to edit numpy & pandas outliers
+def outliers(data_out):
+    out = []
+    count = 0
+    if str(type(data_out))== str("<class 'numpy.ndarray'>"):
+        for col in range(data_out.shape[1]):
+            data = data_out[:,col]
+            print(data)
+
+            quartile_1, quartile_3 = np.percentile(data,[25,75])
+            print("1사분위 : ",quartile_1)
+            print("3사분위 : ",quartile_3)
+            iqr = quartile_3 - quartile_1
+            lower_bound = quartile_1 - (iqr*1.5)
+            upper_bound = quartile_3 + (iqr*1.5)
+            out_col = np.where((data>upper_bound)|(data<lower_bound))
+            print(out_col)
+            data = data[out_col]
+            print(f"{col+1}번째 행렬의 이상치 값: ", data)
+            out.append(out_col)
+            count += len(out_col)
+
+    if str(type(data_out))== str("<class 'pandas.core.frame.DataFrame'>"):
+        i=0
+        print(data_out.columns)
+        for col in data_out.columns:
+            data = data_out[col].values
+            print(data)
+            print(type(data))
+            quartile_1, quartile_3 = np.percentile(data,[25,75])
+            # print("1사분위 : ",quartile_1)
+            # print("3사분위 : ",quartile_3)
+            iqr = quartile_3 - quartile_1
+            lower_bound = quartile_1 - (iqr*1.5)
+            upper_bound = quartile_3 + (iqr*1.5)
+            out_col = np.where((data>upper_bound)|(data<lower_bound))
+            print('===out_col===')
+            print(out_col)
+            print('===out_col[0]===')
+            print(out_col[0], i)
+            data_out.iloc[out_col[0],i]=np.nan
+            data = data[out_col]
+            print(f"'{col}'의 이상치값: ", data)
+            print(type(data))
+            i+=1
+    return data_out
+
+
 ### pd 데이터
 df = pd.DataFrame({'a':[1,2,1000,4,5],
                    'b':[2,3,4,5,6000]})
@@ -45,10 +93,11 @@ print('====a1====')
 
 a1 = outliers(df)
 print(a1)
+print(type(a1))
 
 print('====a2====')
 
-a2 = outliers2(df)
-print(a2)
+# a2 = outliers2(df)
+# print(a2)
 
 print('====--====')
