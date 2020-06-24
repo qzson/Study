ac839a77b9617e15a90abf502a8e8d6675855387
qzson <qzson@naver.com>
2020-06-24 16:23:07 +0900

    데이터 스플릿 함수

---
+++ hamsu/h1_split.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+# test0602_samsung_review에서 사용
+def split_x(seq, size):
+    aaa = []
+    for i in range(len(seq) - size + 1):
+        subset = seq[i:(i+size)]
+        aaa.append([j for j in subset])
+    # print(type(aaa))
+    return np.array(aaa)
+size = 6            # 6일치씩 자르겠다?
+
+
+# test0602_exam에서 사용
+def split_xy3(dataset, time_steps, y_column):
+    x, y = list(), list()
+    for i in range(len(dataset)):
+        x_end_number = i + time_steps
+        y_end_number = x_end_number + y_column
+
+        if y_end_number > len(dataset):
+            break
+        tmp_x = dataset[i:x_end_number, :]
+        tmp_y = dataset[x_end_number:y_end_number, 0]
+        x.append(tmp_x)
+        y.append(tmp_y)
+    return np.array(x), np.array(y)
