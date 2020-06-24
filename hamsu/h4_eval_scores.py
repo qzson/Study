ac1cd520f3b5ad021ad4927d032faaa5e9b670b3
qzson <qzson@naver.com>
2020-06-24 16:24:08 +0900

    ML 분류 모델 결과 함수

---
+++ hamsu/h4_eval_scores.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import accuracy_score, r2_score, recall_score, f1_score, precision_score, roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+def eval_scores(y_test, y_pred):
+    confusion = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    F1 = f1_score(y_test, y_pred)
+    AUC = roc_auc_score(y_test, y_pred)
+    print('오차행렬:\n', confusion)
+    print('\n정확도: {:.4f}'.format(accuracy))
+    print('정밀도: {:.4f}'.format(precision))
+    print('재현율: {:.4f}'.format(recall))
+    print('F1: {:.4f}'.format(F1))
+    print('AUC: {:.4f}'.format(AUC))
