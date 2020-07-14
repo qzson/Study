f7d3bbb4b47caeb8ae31da17049e4a3de1180d20
qzson <qzson@naver.com>
2020-07-14 20:13:01 +0900

    pyo + web

---
+++ flask/fk13_myweb.py
@@ -0,0 +1,40 @@
+# 20-07-14_35
+# 암기를 해라 그냥
+
+import pyodbc as pyo
+
+server = '127.0.0.1'
+database = 'bitdb'
+username = 'bit2'
+password = '3411'
+
+conn = pyo.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER=' +server+   # 서버 연결
+                   '; PORT=1433; DATABASE=' +database+
+                   '; UID=' +username+
+                   '; PWD=' +password)
+
+cursor = conn.cursor()
+
+tsql = 'SELECT * FROM iris2;'
+
+# with cursor.execute(tsql):
+#     row = cursor.fetchone()
+
+#     while row :
+#         print(str(row[0]) + '' + str(row[1]) + '' + str(row[2]) + '' +
+#               str(row[3]) + '' + str(row[4]))
+#         row = cursor.fetchone()
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/sqltable')
+def showsql():
+    cursor.execute(tsql)                                            # SELECT * FROM iris2를 실행
+    return render_template('myweb.html', rows = cursor.fetchall())  # myweb에 rows를 보냄
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=False)
+
+conn.close()
